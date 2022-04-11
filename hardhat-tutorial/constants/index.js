8ace0ba603794a1a36025fe46139abb24ec190b8
LisandroDelReal <lldelreal@outlook.com>
2022-04-11 16:57:20 -0300

    added contract and app

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0xC4CBdD1c369B529bf354F389Bcb603654aDd4764";
+
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
